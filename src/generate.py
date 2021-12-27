d0c0489fc10a365d76bd20a750f8a5403ba70c84
d-asnaghi <asnaghi@me.com>
2021-12-27 02:07:22 +0100

    [SRC] first drop

---
+++ src/generate.py
@@ -0,0 +1,59 @@
+import json
+import argparse
+from pathlib import Path
+import rocks
+
+
+def json_config(name, description, address, png):
+    return json.dumps({
+        "name": f"{name}",
+        "symbol": "",
+        "description": f"{description}",
+        "image": f"{png}",
+        "seller_fee_basis_points": 0,
+        "collection": {
+            "name": "Rocks X NFT",
+            "family": "Rocks"
+        },
+        "properties": {
+            "files": [
+                {
+                    "uri": f"{png}",
+                    "type": "image/png"
+                }
+            ],
+            "creators": [
+                {
+                    "address": f"{address}",
+                    "share": 100
+                }
+            ]
+        }
+    }, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", type=Path, help="output dir")
+    parser.add_argument("-s, --start", type=int, default=0, help="sequence start")
+    args = parser.parse_args()
+
+    for n in range(args.start, 5):
+
+        config_path = args.dir / f"{n}.json"
+        image_path = args.dir / f"{n}.png"
+
+        attributes = rocks.attributes()
+        image, metadata = rocks.rock(attributes)
+
+        description = f"""{metadata["color"]} {metadata["style"]} Rock. Grown with love and affection"""
+
+        with open(config_path, "w") as cfg:
+            cfg.write(json_config(
+                name=f"Test Friendly Rock #{n}",
+                description=description,
+                address="2Wbf3ZCrXzsKCRPSfbAmJ7gddEubwMqkhvBD7jeb9DmY",
+                png=image_path.stem
+            ))
+
+        image.save(image_path)
---
+++ src/rocks.py
@@ -0,0 +1,77 @@
+
+from PIL import Image
+import random
+from pathlib import Path
+import os
+
+
+def base() -> Image.Image:
+    layer = Image.open("src/layers/base.png")
+    return layer.convert('RGBA')
+
+
+def attr_name(path: Path) -> str:
+    return path.stem.removeprefix(path.parent.stem).replace("_", " ").strip().title()
+
+
+def attributes() -> dict:
+    layers = Path("src/layers")
+    attributes = {}
+
+    categories = [Path(d) for d in os.scandir(layers) if d.is_dir()]
+    for category in categories:
+        images = category.glob('*.png')
+        attributes[category.stem] = [(attr_name(img), img) for img in images]
+
+    return attributes
+
+
+def random_color():
+    colors = ["red", "purple", "green", "yellow", "black"]
+    return random.choice(colors)
+
+
+def color_overlay(image: Image, color, mask: Image = None, alpha: float = 0.2) -> Image:
+    image = Image.blend(image, Image.new('RGBA', image.size, color), alpha)
+
+    if mask:
+        r, g, b, _ = image.split()
+        _, _, _, a = mask.split()
+        return Image.merge('RGBA', (r, g, b, a))
+    return image
+
+
+def hat(n):
+    hats = [f for f in Path("src/layers/hats").glob('*')]
+    return Image.open(hats[n])
+
+
+def nft(base: Image.Image, attrs: dict):
+    image = base
+    metadata = {}
+
+    # Add a color overlay
+    metadata["color"] = random_color().title()
+    image = color_overlay(image, metadata["color"], mask=image)
+
+    # Paste attributes
+    for attribute, values in attrs.items():
+        name, path = random.choice(values)
+        metadata[attribute] = name
+        layer = Image.open(path)
+        image.paste(layer, (0, 0), mask= layer)
+
+    return image, metadata
+
+
+def rock(attrs: dict):
+    return nft(base(), attrs)
+
+
+if __name__ == "__main__":
+
+    attrs = attributes()
+    for n in range(0, 3):
+        image, metadata = rock(attrs)
+        print(metadata)
+        image.show()
