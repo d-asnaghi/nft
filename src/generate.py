95890d01be7049484d23419daec7c406146e5ace
d-asnaghi <asnaghi@me.com>
2021-12-31 01:06:01 +0100

    [SRC] shorter description

--- src/generate.py
+++ src/generate.py
@@ -24,7 +24,8 @@ def json_config(name, description, attributes, address, png):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=Path, help="output dir")
-    parser.add_argument("--number", type=int, default=10, help="number of images")
+    parser.add_argument("--number", type=int, default=10,
+                        help="number of images")
     args = parser.parse_args()
 
     # Cache the available traits
@@ -37,12 +38,11 @@ if __name__ == "__main__":
 
         image, attributes = rocks.rock(traits)
 
-        labels = {}
-        for entry in attributes:
-            trait, value = entry["trait-type"], entry['value']
-            labels[trait.lower()] = value.lower()
+        labels = {trait.lower(): value.lower() for trait, value in [
+            (entry["trait-type"], entry['value']) for entry in attributes
+        ]}
 
-        description = f"A friendly {labels['profession']} rock, made of {labels['material']}"
+        description = f"A {labels['profession']} rock, made of {labels['material']}"
 
         with open(config_path, "w") as cfg:
             cfg.write(json_config(
